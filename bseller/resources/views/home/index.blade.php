bd0a40d32efaadf4227f9702d74ede81f9f12d5e
scorreah <scorreah@eafit.edu.co>
2023-02-25 18:04:15 -0500

    Basic Project Configuration

---
+++ bseller/resources/views/home/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home Page - BSeller')
+@section('content')
+<h1>Welcome to BSeller!</h1>
+<p>This is a reseller of unique shoes!</p>
+@endsection
---
+++ bseller/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+    <title>@yield('title', 'BSeller')</title>
+</head>
+<body>
+    <nav>
+		<ul>
+			<li><a href="{{ route('home.index') }}">Home</a></li>
+			<li><a href="#">Shoes</a></li>
+			<li><a href="#">Orders</a></li>
+			<li><a href="#">Bids</a></li>
+		</ul>
+	</nav>
+    @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</body>
+</html>

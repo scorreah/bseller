abbcd1e6cb3dff765792367581689ec85b59d593
dgomezc1 <dgomezc10@eafit.edu.co>
2023-03-08 13:07:21 -0500

    Eliminacion de metodo de guardar imagen en controlador

--- bseller/app/Models/Shoe.php
+++ bseller/app/Models/Shoe.php
@@ -107,18 +107,4 @@ class Shoe extends Model
         return $validatedData;
     }
 
-    public function saveImage(Request $request) : string
-    {   
-        try
-        {
-            $image_shoe = $request->file('image_shoe');
-            $nombreImagen =  "img\shoes\\".time()."_".$image_shoe->getClientOriginalName();
-            Storage::disk('local')->put($nombreImagen,  File::get($image_shoe));
-        }catch(Exception $e)
-        {
-            $nombreImagen = "Error";
-        }
-        return $nombreImagen;
-    }
-
 }

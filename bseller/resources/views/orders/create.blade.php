4812ce49b3729be3a4799ac9f6e9e3bf50a79e8f
smenesesd <smenesesd@eafit.edu.co>
2023-03-08 23:46:54 -0500

    Cambios en el create.blade con css

--- bseller/resources/views/orders/create.blade.php
+++ bseller/resources/views/orders/create.blade.php
@@ -1,43 +1,75 @@
 @extends('layouts.app')
 @section('content')
-<div class="row">
-    <div class="col-md-8 offset-md-2">
-        <div class="card">
-            <div class="card-header">
-                <h4>Create Order</h4>
+    <div class="row">
+        <div class="col-md-8 offset-md-0.5>
+            <div class="title">
+                <h2>Create Order </h2>
             </div>
-            @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-            <div class="card-body">
-                <form method="POST" action="{{ route('orders.store') }}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="total_price">Total Price:</label>
-                        <input type="number" class="form-control @error('total_price') is-invalid @enderror" id="total_price" name="total_price" value="{{ old('total_price') }}" required>
-                        @error('total_price')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+            <div class ="d-flex flex-col justify-content-center">
+                <div class="card">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <style>
+                        .title {
+                            display: flex;
+                            text-align: left;
+                            vertical-align: top;
+                            top: 0;
+                            left: 0;
+                            padding: 5px;
+                        }
+
+                        .card {
+                            display: flex;
+                            width: 40%;
+                            margin: 15% auto;
+                            border: solid 1px transparent;
+                        }
+
+                        button{
+                            margin-top: 10px;
+                        }
+                        form {
+                            width: 50%;
+                            margin: 0 auto;
+                        }
+                    </style>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('orders.store') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="total_price">Total Price:</label>
+                                <input type="number" class="form-control @error('total_price') is-invalid @enderror"
+                                    id="total_price" name="total_price" value="{{ old('total_price') }}" required>
+                                @error('total_price')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="status">Status:</label>
+                                <select class="form-control @error('status') is-invalid @enderror" id="status"
+                                    name="status" required>
+                                    <option value="">-- Select Status --</option>
+                                    <option value="new" @if (old('status') == 'new') selected @endif>New</option>
+                                    <option value="processing" @if (old('status') == 'processing') selected @endif>Processing
+                                    </option>
+                                    <option value="completed" @if (old('status') == 'completed') selected @endif>Completed
+                                    </option>
+                                    <option value="cancelled" @if (old('status') == 'cancelled') selected @endif>Cancelled
+                                    </option>
+                                </select>
+                                @error('status')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <label for="status">Status:</label>
-                        <select class="form-control @error('status') is-invalid @enderror" id="status" name="status" required>
-                            <option value="">-- Select Status --</option>
-                            <option value="new" @if(old('status') == 'new') selected @endif>New</option>
-                            <option value="processing" @if(old('status') == 'processing') selected @endif>Processing</option>
-                            <option value="completed" @if(old('status') == 'completed') selected @endif>Completed</option>
-                            <option value="cancelled" @if(old('status') == 'cancelled') selected @endif>Cancelled</option>
-                        </select>
-                        @error('status')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-primary">Create</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection

b697fc77bc5f9e77ed60bbe6c49b110a2f3e971a
dgomezc10 <dgomezc10@eafit.educ.co>
2023-03-19 23:31:18 -0500

    Creacion de middleware de admin

--- bseller/app/Http/Middleware/AdminAuthMiddleware.php
+++ bseller/app/Http/Middleware/AdminAuthMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuthMiddleware
 {
@@ -15,6 +16,12 @@ class AdminAuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if (Auth::user() && Auth::user()->getIsAdmin()) 
+        {
+            return $next($request);
+        } else 
+        {
+            return redirect()->route('home.index');
+        }
     }
 }

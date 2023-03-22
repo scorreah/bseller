fa6b428760817a62f071d0e87a977cce54dfe930
dgomezc1 <dgomezc10@eafit.edu.co>
2023-03-22 11:10:46 -0500

    Correcion de modelos y ajustes de orders para unicamente mostrar las orders correspondientes al usuario

--- bseller/resources/views/order/index.blade.php
+++ bseller/resources/views/order/index.blade.php
@@ -1,12 +1,33 @@
 @extends('layouts.app')
 @section('title', $viewData["title"])
 @section('content')
-    <h1>This is the Orders page</h1>
-    <p>In this page you're gonna see all your orders made!</p>
-    <center>
-        <div class="container">
-            <a href="{{ route('order.create') }}" class ="btn btn-primary">Create Order</a>
-            <a href="{{ route('order.list') }}" class ="btn btn-primary">List Orders</a>
+
+@if (session('status'))
+  <div class="alert alert-success">
+    {{ session('status') }}
+  </div>
+@endif
+
+    <div class="container">
+        <h1>Orders List</h1>
+        <p>In this page you're gonna see all your orders made!</p>
+        <div class="row">
+            <tbody>
+                @if(count($viewData['orders'])<=0)
+                    <p>At the moment you dont have any order :(</p>
+                @else
+                    @foreach ($viewData['orders'] as $order)
+                        <tr>
+                            <td>
+                                <a href="{{ route('order.show', ['id'=> $order["id"]]) }}" class="btn bg-primary text-white">Order ID {{ $order["id"] }}</a>
+                                </form>
+                                <p class="card-text">Total Price: {{ $order->total_price }}</p>
+                                <p class="card-text">Status: {{ $order->status }}</p>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
         </div>
-    </center>
+    </div>
 @endsection

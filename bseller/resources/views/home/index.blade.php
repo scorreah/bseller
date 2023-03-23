cc8284141aacd75a6836457908f8b19e1da2d5fd
smenesesd <smenesesd@eafit.edu.co>
2023-03-23 16:59:28 -0500

    Mejora del span en index.blade y cambio de img a vectoirzada

--- bseller/resources/views/home/index.blade.php
+++ bseller/resources/views/home/index.blade.php
@@ -2,11 +2,13 @@
 @section('title', 'Home Page - BSeller')
 @section('content')
 
+<div class = "mainText">
+    <div class="secondText">
+        <span>WELCOME TO BSELLER!</span>
+    </div>
+</div>
 <div class="navImage">
-    <img src="/img/index/navImagePurple.png" width="900px" height="300px" alt="">
+    <img src="/img/index/navImagePurple.svg" width="1500px" height="400px" alt="">
 </div>
 
-<div class="secondText">
-    <span>Bsellerrrrrrrrrrrr</span>
-</div>
 @endsection

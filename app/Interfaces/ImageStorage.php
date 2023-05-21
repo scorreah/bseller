df4b6a598cbe71c51c118da23e148bceeb81db62
scorreah <scorreah@eafit.edu.co>
2023-05-20 19:43:54 -0500

    Re-añadiendo Interfaces/

---
+++ app/Interfaces/ImageStorage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Http\Request;
+
+interface ImageStorage
+{
+    public function store(Request $request): string;
+
+    public function delete(string $dir): bool;
+}

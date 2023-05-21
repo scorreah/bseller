1259aa3d1b7b19f980b6fa1500547a142b832fe4
smenesesd <smenesesd@eafit.edu.co>
2023-05-21 12:47:05 -0500

    Profile routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
 
 // Profile Routes
-// Route::get('/profile', 'App\Http\Controllers\ProfileController@index')->name('profile.index');
 Route::get('/profile', 'App\Http\Controllers\ProfileController@show')->name('profile.show');
 
 // Bid Routes

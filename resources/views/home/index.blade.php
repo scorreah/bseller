5170e529daa31ba1e952b9d43b1b2d81e9440c8f
scorreah <scorreah@eafit.edu.co>
2023-05-20 19:41:42 -0500

    Test

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
 <div class="main">
     <div class = "mainText">
         <div class="secondText">
-            <span>{{ __('home.welcome') }} - CICD</span>
+            <span>{{ __('home.welcome') }}</span>
         </div>
     </div>
 
